1ef1465fb3a91f089850a9e3012e549dd76ebcf1
Yahir <yahiratzin@gmial.com>
2022-07-07 13:59:16 -0500

    Crear las vistas login, registro y contraseña

---
+++ controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+
+class LoginController{
+    public static function login(){
+        echo 'desde Login';
+    } 
+
+    public static function logout(){
+        echo 'desde logout';
+    }
+
+    public static function olvide(){
+        echo 'desde olvide';
+    }
+
+    public static function recuperar(){
+        echo 'desde recuperar';
+    }
+
+    public static function registro(){
+        echo 'desde registro';
+    }
+}
+   
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd7fb7b8c7a141af775695584d3d5930ac77eca46',
+        'name' => 'yahir/dimas-mantenimiento',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yahir/dimas-mantenimiento' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd7fb7b8c7a141af775695584d3d5930ac77eca46',
+            'dev_requirement' => false,
+        ),
+    ),
+);
